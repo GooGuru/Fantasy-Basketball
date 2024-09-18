117ab3885c8543e5c838dfcf850626dc4d9dc077
Chris <ccolon29@mail.valenciacollege.edu>
2024-09-17 20:06:37 -0400

    created the index file under models, added the paths for each of the models and exported the module. created the team model.

---
+++ server/models/Team.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require("mongoose");
+
+const playerSchema = require("./Player");
+
+const teamSchema = new Schema(
+  {
+    teamName: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    teamPoints: {
+      type: Number,
+    },
+    players: [playerSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+const Team = model('Team', teamSchema);
+
+model.exports = Team;
---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const League = require('./League');
+const Player = require('./Player');
+const Team = require('./Team');
+const User = require('./User');
+
+module.exports = { League, Player, Team, User };
