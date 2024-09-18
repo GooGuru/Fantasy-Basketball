57651f6bcfabe9e1dcd5e3dc8c7dbe8475602871
Jack <108418371+ZolarEclipse@users.noreply.github.com>
2024-09-18 10:23:49 -0500

    started a homepage. run 'npm run dev' in the client folder to show a homepage with nav links

---
+++ client/src/components/Navigation.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import '../styles/Navigation.css';
+
+const Navigation = () => {
+  return (
+    <nav className="navbar">
+      <ul className="nav-list">
+        <li className="nav-item">
+          <NavLink to="/league" className="nav-link" activeClassName="active-link">
+            League
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/team" className="nav-link" activeClassName="active-link">
+            Team
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
