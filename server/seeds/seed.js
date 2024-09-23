1adc5defdc99ca3577939b4f03ee80865a9126d1
Chris <ccolon29@mail.valenciacollege.edu>
2024-09-23 14:48:46 -0400

    added seed data

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,13 +1,17 @@
 const db = require('../config/connection');
 const  { User } = require('../models');
 const { League } = require('../models');
-const { Teams } = require('../models');
+const { Team } = require('../models');
+const { Player } = require('../models');
 const cleanDB = require('./cleanDB');
 
+const userData = require('./userData.json');
 const leagueData = require('./leagueData.json');
 const teamData = require('./teamData.json');
-const userData = require('./userData.json');
+const playerData = require('./playerData.json');
 
 db.once('open', async () => {
-    await cleanDB()
+    await cleanDB();
+
+    await 
 })
