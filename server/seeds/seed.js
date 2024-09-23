22e46a9f60945f86b075a41d929e124012713af8
Chris <ccolon29@mail.valenciacollege.edu>
2024-09-23 15:06:27 -0400

    created the function to clean db and seed with seed data.

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,17 +1,30 @@
-const db = require('../config/connection');
-const  { User } = require('../models');
-const { League } = require('../models');
-const { Team } = require('../models');
-const { Player } = require('../models');
-const cleanDB = require('./cleanDB');
+const db = require("../config/connection");
+const { User } = require("../models");
+const { League } = require("../models");
+const { Team } = require("../models");
+const { Player } = require("../models");
+const cleanDB = require("./cleanDB");
 
-const userData = require('./userData.json');
-const leagueData = require('./leagueData.json');
-const teamData = require('./teamData.json');
-const playerData = require('./playerData.json');
+const userData = require("./userData.json");
+const leagueData = require("./leagueData.json");
+const teamData = require("./teamData.json");
+const playerData = require("./playerData.json");
 
-db.once('open', async () => {
-    await cleanDB();
+db.once("open", async () => {
+  try {
+    await cleanDB('User', 'users');
+    await cleanDB('League', 'leagues');
+    await cleanDB('Team', 'teams');
+    await cleanDB('Player', 'players');
 
-    await 
-})
+    await User.insertMany(userData);
+    await League.insertMany(leagueData);
+    await Team.insertMany(teamData);
+    await Player.insertMany(playerData);
+
+    console.log("Data has been seeded.");
+    process.exit(0);
+  } catch (err) {
+    throw err;
+  }
+});
