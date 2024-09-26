83afd34c4c819f0195d68bf9c422e7af614c7a5a
Jack <108418371+ZolarEclipse@users.noreply.github.com>
2024-09-26 00:07:54 -0500

    player have cards and look nice

--- client/src/components/League.jsx
+++ client/src/components/League.jsx
@@ -1,57 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 const League = () => {
-  const [players, setPlayers] = useState([]);
-
-  useEffect(() => {
-    const fetchPlayers = async () => {
-      const myHeaders = new Headers();
-      myHeaders.append("x-rapidapi-key", "28fb5b56deb9822911fc30b14c38aeeb");
-      myHeaders.append("x-rapidapi-host", "v3.football.api-sports.io");
-
-      const requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
-      };
-
-      try {
-        const response = await fetch("https://v3.football.api-sports.io/players?league=39&season=2022", requestOptions);
-        const data = await response.json();
-
-        const playersArray = data.response.map(playerData => {
-          const player = playerData.player;
-          const position = playerData.statistics[0]?.games?.position || "Unknown";
-          return {
-            id: player.id,
-            name: `${player.firstname} ${player.lastname}`,
-            position: position
-          };
-        });
-
-        setPlayers(playersArray); 
-      } catch (error) {
-        console.error('Error fetching players:', error);
-      }
-    };
-
-    fetchPlayers();
-  }, []); 
-
+  console.log("League component rendered");
   return (
     <div>
       <h1>Your Leagues</h1>
-      {players.length > 0 ? (
-        <ul>
-          {players.map(player => (
-            <li key={player.id}>
-              {player.name} - {player.position}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>Loading players...</p>
-      )}
+
     </div>
   );
 };
